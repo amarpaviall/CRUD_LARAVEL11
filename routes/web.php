00205a3e44b9b67270b536bd874890b466a652db
amarpaviall <er.amarpaviall@gmail.com>
2024-10-28 17:33:08 -0400

    update web.php
    one way to do jobs as make jobs array one level upper and use that variable in route function using "use" keyword

--- routes/web.php
+++ routes/web.php
@@ -3,51 +3,36 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+$jobs = [
+    [
+        'id' => 1,
+        'title' => 'Director',
+        'salary' => 40000,
+    ],
+    [
+        'id' => 2,
+        'title' => 'Developer',
+        'salary' => 30000,
+    ],
+    [
+        'id' => 3,
+        'title' => 'Teacher',
+        'salary' => 25000,
+    ],
+];
+
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
+Route::get('/jobs', function () use ($jobs) {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => 40000,
-            ],
-            [
-                'id' => 2,
-                'title' => 'Developer',
-                'salary' => 30000,
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => 25000,
-            ],
-        ]
-
+        'jobs' => $jobs
     ]);
 });
 
-Route::get('/job/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => 40000,
-        ],
-        [
-            'id' => 2,
-            'title' => 'Developer',
-            'salary' => 30000,
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => 25000,
-        ],
-    ];
+Route::get('/job/{id}', function ($id) use ($jobs) {
+    $jobs = $jobs;
     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
     //dd($job);
 
