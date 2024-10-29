1fcf388fd507577bb4bdade0c2b611781e5e6d39
amarpaviall <er.amarpaviall@gmail.com>
2024-10-28 22:49:36 -0400

    tags belongs to many jobs

--- app/Models/Tags.php
+++ app/Models/Tags.php
@@ -9,4 +9,10 @@ class Tags extends Model
 {
     /** @use HasFactory<\Database\Factories\TagsFactory> */
     use HasFactory;
+
+    public function jobs()
+    {
+        //our table has job_listing_id instead of job_id as there is relation so relatedPivotKey opposite of job model page
+        return $this->belongsToMany(Job::class, relatedPivotKey: "job_listing_id");
+    }
 }
